f63be30fe87a0f8178c07b341a37486396f79eb5
Florian Meyer <florian.meyer@capgemini.com>
2019-10-09 13:18:12 +0200

    Interpreter and basic class structure

--- PietPad/MainWindow.xaml.cs
+++ PietPad/MainWindow.xaml.cs
@@ -26,19 +26,6 @@ namespace PietPad
         public MainWindow()
         {
             InitializeComponent();
-            var i = new Interpreter();
-            var image = new Codel[3, 2] { { new Codel(0, 0), new Codel(0, 1) }, { new Codel(1, 0), new Codel(1, 1) }, { new Codel(2, 0), new Codel(2, 1) } };
-            image[0, 0].Color = CodelColor.LightRed;
-            image[1, 0].Color = CodelColor.Red;
-            image[2, 0].Color = CodelColor.DarkMagenta;
-            var stdout = new MemoryStream();
-            var stdin = new MemoryStream();
-            i.Interpret(image, stdout, stdin);
-            using (var sr = new StreamReader(stdout))
-            {
-                stdout.Seek(0, SeekOrigin.Begin);
-                MessageBox.Show(sr.ReadToEnd());
-            }
         }
     }
 }
